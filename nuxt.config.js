4006384502324664a31d90a7ab9dd55625da7221
max <ahmet.ugur@std.gau.edu.tr>
2023-03-09 00:26:46 +0200

    api proxy changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -115,7 +115,7 @@ export default {
 
   proxy: {
     "/api/": {
-      target: "https://wings.rentals/"
+      target: "https://office.wingss.xyz/"
     },
     "/data": {
       target: "http://93.115.79.123/"
