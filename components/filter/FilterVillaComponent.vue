0c5cfad0782944da35e3d78d4fa89205ac277227
max <ahmet.ugur@std.gau.edu.tr>
2023-03-20 02:33:18 +0300

    fix deneme 9

--- components/filter/FilterVillaComponent.vue
+++ components/filter/FilterVillaComponent.vue
@@ -64,7 +64,10 @@
           <div class="Filter-right-selected">
             <div class="Filter-right-selected-in">
 
-              {{selectedDestinations}}
+              <a v-for="destination in selectedDestinations" class="Filter-right-selected-item">
+                Bölge:{{ destination.text }}
+                <i class="icon-search-close" @click="unselect(destination)"></i>
+              </a>
 
               <a v-for="facilityType in selectedFacilityTypes" class="Filter-right-selected-item">
                 Bölge:{{ facilityType.text }}
@@ -81,7 +84,6 @@
                 <i class="icon-search-close" @click="unselect(facility)"></i>
               </a>
 
-              {{filterCount}}
             </div>
           </div>
 
