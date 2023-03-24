09e22843b5b433974d849e0282cbb8198e9b9168
max <ahmet.ugur@std.gau.edu.tr>
2023-03-24 03:12:25 +0300

    301 fix

--- middleware/301.js
+++ middleware/301.js
@@ -1,24 +1,9 @@
 export default function ({route, redirect}) {
   let site_id = process.env.SITE;
 
-  let redirects = [];
+  const redirects = require('./301_sites/' + site_id + '.js')
 
-  if (site_id === "1") {
-    redirects = [
-      // Eski url, Yeni url
-      ['/eski', '/kiralik-villa'],
-      ['/eski-yol-2', '/yeni-yol-2'],
-    ];
-  }
-
-  if (site_id === "2") {
-    redirects = [
-      ['/eski', 'http://google.com'],
-      ['/eski2', '/kiralik-villa']
-    ]
-  }
-
-  const matchedRedirect = redirects.find(([oldPath]) => oldPath === route.path);
+  const matchedRedirect = redirects.default.find(([oldPath]) => oldPath === route.path);
   if (matchedRedirect) {
     const [, newPath] = matchedRedirect;
     return redirect(301, newPath);
