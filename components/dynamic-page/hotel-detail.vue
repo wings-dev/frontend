f93455f3848076fea28a8750187d8ef47d242e27
max <ahmet.ugur@std.gau.edu.tr>
2023-03-31 01:45:10 +0300

    zero index fix

--- components/dynamic-page/hotel-detail.vue
+++ components/dynamic-page/hotel-detail.vue
@@ -153,7 +153,6 @@
 
       <section class="rooms-section pt-3 mt-1">
         <div class="container">
-          {{hotelPriceDetails.body?.hotels[0]?.offers}}
           <template v-if="roomsLoading">
             Odalar yükleniyor...
           </template>
@@ -512,8 +511,14 @@
       <div class="otel-features">
         <div class="container">
           <div class="otel-features-in">
-            <h4>{{ hotelDetails.body.hotel.name }} <span>Özellikler</span></h4>
-            <p class="otel-features-item" v-for="featuresitem in hotelDetails.body.hotel.seasons[0].facilityCategories[0].facilities" :key="featuresitem.id">{{ featuresitem.name }} <span v-if="featuresitem.isPriced" style="font-weight:500;text-decoration:underline;margin-left: 5px;">(Ücretli)</span></p>
+            <h4>{{ hotelDetails.body.hotel?.name }} <span>Özellikler</span></h4>
+            <template v-for="season in hotelDetails.body?.hotel?.seasons">
+              <template v-for="facilityCategory in season.facilityCategories">
+                <p class="otel-features-item" v-for="facility in facilityCategory.facilities" :key="facility.id">{{ facility.name }}
+                  <span v-if="facility.isPriced" style="font-weight:500;text-decoration:underline;margin-left: 5px;">(Ücretli)</span>
+                </p>
+              </template>
+            </template>
           </div>
         </div>
       </div>
@@ -665,105 +670,6 @@
         </div>
       </b-modal>
     </main>
-
-    <section style="display: none" class="rooms-section pt-3 mt-1">
-      <div class="container">
-        <div v-for="offer in hotelPriceDetails.body?.hotels[0]?.offers"
-          class="room border border-light rounded-xxl p-2 mb-3">
-          <div class="row">
-            {{ offer }}
-            <div class="col-12 col-lg-8 col-xl-7 d-flex flex-column flex-sm-row pe-xl-4 mb-lg-0 mb-2">
-              <div class="img-box position-relative flex-shrink-0">
-                <img src="https://boceksoft.com/works/otelbnb/cdn/img/spacer.gif"
-                  data-src="https://boceksoft.com/works/otelbnb/cdn/uploads/room-image1.jpg" width="281" height="186"
-                  alt="room image" class="lazy cover flex-shrink-0 rounded-xl loaded"
-                  style="background-image: url(&quot;https://boceksoft.com/works/otelbnb/cdn/uploads/room-image1.jpg&quot;);">
-              </div>
-              <div class="flex-fill d-flex flex-column align-items-start ps-2 ps-sm-3 ps-xl-4 pt-3">
-                <div class="d-flex align-items-center w-100 mb-2">
-                  <span class="fs-5 lh-sm ls-05 d-inline-block me-auto">Standart Oda</span>
-                  <a href="javascript:void(0);"
-                    class="bg-transparent text-theme-secondary fs-6 ls-05 d-flex align-items-center p-0">
-                    <img src="https://boceksoft.com/works/otelbnb/cdn/img/icons/024-calendar-second.svg" width="18"
-                      height="20" alt="calendar" class="lazy contain flex-shrink-0 me-1">
-                    <small><u>Müsaitlik Takvimi</u></small>
-                  </a>
-                </div>
-                <div
-                  class="info fs-6 bg-theme-light d-flex align-items-center rounded-sm text-theme-secondary py-2 px-3 w-100 mb-3">
-                  <img src="https://boceksoft.com/works/otelbnb/cdn/img/spacer.gif"
-                    data-src="https://boceksoft.com/works/otelbnb/cdn/img/icons/015-bed-second.svg" width="18" height="18"
-                    alt="group" class="lazy contain flex-shrink-0 me-2 loaded"
-                    style="background-image: url(&quot;https://boceksoft.com/works/otelbnb/cdn/img/icons/015-bed-second.svg&quot;);">
-                  <small class="lh-sm ls-05">En fazla 3 yetişkin veya 2 yetişkin + 2 çocuk</small>
-                </div>
-                <div class="room-highlights d-flex flex-wrap w-100 fs-6 lh-sm mb-2 mb-sm-3">
-                  <small
-                    class="hl-item d-flex align-items-center justify-content-sm-start justify-content-between ls-05 me-sm-3 pe-2 pe-sm-1 mb-2 mb-sm-1">
-                    <span class="text-theme-secondary me-1">Oda Genişliği</span>
-                    <span class="">53 m<sup><small>2</small></sup></span>
-                  </small>
-                  <small
-                    class="hl-item d-flex align-items-center justify-content-sm-start justify-content-between ls-05 me-sm-3 pe-2 pe-sm-1 mb-2 mb-sm-1">
-                    <span class="text-theme-secondary me-1">Minibar</span>
-                    <img src="https://boceksoft.com/works/otelbnb/cdn/img/spacer.gif"
-                      data-src="https://boceksoft.com/works/otelbnb/cdn/img/icons/025-tick-second.svg" width="18"
-                      height="18" alt="" class="lazy contain loaded"
-                      style="background-image: url(&quot;https://boceksoft.com/works/otelbnb/cdn/img/icons/025-tick-second.svg&quot;);">
-                  </small>
-                  <small
-                    class="hl-item d-flex align-items-center justify-content-sm-start justify-content-between ls-05 me-sm-3 pe-2 pe-sm-1 mb-2 mb-sm-1">
-                    <span class="text-theme-secondary me-1">Balkon</span>
-                    <img src="https://boceksoft.com/works/otelbnb/cdn/img/spacer.gif"
-                      data-src="https://boceksoft.com/works/otelbnb/cdn/img/icons/025-tick-second.svg" width="18"
-                      height="18" alt="" class="lazy contain loaded"
-                      style="background-image: url(&quot;https://boceksoft.com/works/otelbnb/cdn/img/icons/025-tick-second.svg&quot;);">
-                  </small>
-                  <small
-                    class="hl-item d-flex align-items-center justify-content-sm-start justify-content-between ls-05 me-sm-3 pe-2 pe-sm-1 mb-2 mb-sm-1">
-                    <span class="text-theme-secondary me-1">Duşakabin</span>
-                    <img src="https://boceksoft.com/works/otelbnb/cdn/img/spacer.gif"
-                      data-src="https://boceksoft.com/works/otelbnb/cdn/img/icons/025-tick-second.svg" width="18"
-                      height="18" alt="" class="lazy contain loaded"
-                      style="background-image: url(&quot;https://boceksoft.com/works/otelbnb/cdn/img/icons/025-tick-second.svg&quot;);">
-                  </small>
-                </div>
-                <div class="d-flex align-items-center mb-sm-0 mb-1">
-                  <a href="#!" class="fs-6 lh-sm ls-05 me-4 pe-2 pe-sm-3">
-                    <u><small>Odanın Tüm Özellikleri</small></u>
-                  </a>
-                  <a href="#!" class="fs-6 lh-sm ls-05">
-                    <u><small>Tüm Fiyatlar</small></u>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="room-scroll-wrapper col-12 col-lg-4 col-xl-5 ps-lg-0">
-              <div class="room-options d-flex flex-lg-column pb-lg-0 pb-1">
-                <div
-                  class="option d-flex flex-column flex-xl-row rounded-xl bg-theme-light-2 flex-fill overflow-hidden mb-lg-1">
-                  <div class="flex-fill d-flex flex-column justify-content-center fs-6 ps-3 ps-xl-4 pe-3 py-4">
-                    <span class="d-block text-theme-first lh-1 ls-05 mb-2">Oda Kahvaltı</span>
-                  </div>
-                  <div
-                    class="bg-white bg-opacity-50 d-flex flex-xl-column align-items-center justify-content-center px-3 px-xl-4 py-3 py-xl-2">
-                    <div class="d-flex flex-column align-items-xl-center me-xl-0 me-auto">
-                      <span class="text-secondary ls-05 d-block lh-1"><small>{{ offer.night }} GECE</small></span>
-                      <strong class="fs-5 fw-semibold d-block mb-xl-3 pb-xl-1 lh-sm">{{ offer.price.amount }}
-                        {{ offer.price.currency }}</strong>
-                    </div>
-                    <button type="button"
-                      class="btn btn-theme-first btn-ripple fw-medium ls-05 lh-sm rounded-pill px-3 px-xl-4 py-2">
-                      <small class="d-block py-1">Rezervasyon</small>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
   </div>
 </template>
 
@@ -805,7 +711,7 @@ export default {
 
       console.log(JSON.stringify(response.data));
 
-      this.getRoomImages(this.hotelPriceDetails.body?.hotels[0]?.offers[0].rooms[0].roomId)
+      // this.getRoomImages(this.hotelPriceDetails.body?.hotels[0]?.offers[0].rooms[0].roomId)
     }
 
 
