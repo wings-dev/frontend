5267f980b9d629c1f989c1b268be6f5bcf71caa0
cumalikaya <cali.kaya3@gmail.com>
2023-04-12 13:40:30 +0300

    blog console.loglar temizlendi

--- pages/blog/_category/_slug.vue
+++ pages/blog/_category/_slug.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <dynamic-blog-detail :data="pageData" :morePost ="pagePosts"></dynamic-blog-detail>
+    <dynamic-blog-detail :data="pageData" :morePost="pagePosts"></dynamic-blog-detail>
     <!-- <blog-detail></blog-detail> -->
   </div>
 </template>
@@ -23,22 +23,16 @@ export default {
 
     const response = await $axios.get((process.server ? 'http://localhost:' + process.env.NODE_PORT : '') + `/website/blogs?api_token=${process.env.WEBSITE_TOKEN}`)
     let pagePosts = response.data.data;
-
-
-
+    
     const pageURLs = Object.keys(store.state.routes.routes)
       .filter(key => store.state.routes.routes[key].type === 14)
       .filter(key => key == params.category)
 
-    console.log(params.slug)
-
     pagePosts = response.data.data.filter(item => item.page_content.blog_category.includes(String(pageURLs))).filter(item => item.url !== params.slug)
     // pagePosts = pagePosts.filter(item => )
-    console.log()
     return { pageData, pagePosts }
   },
   mounted() {
-    console.log(this.pagePosts)
   }
 
 }
