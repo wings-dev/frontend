c0513a392f200a75f4f23653ef76479b14c50af9
max <ahmet.ugur@std.gau.edu.tr>
2023-05-01 11:41:39 +0300

    wilcard 301 odeme-yap

--- middleware/301.js
+++ middleware/301.js
@@ -1,14 +1,26 @@
 export default function ({route, redirect}) {
   let site_id = process.env.SITE;
 
-  if (route.path.startsWith('/reservation')) {
-    const newDomain = process.env.OLD_DOMAIN;
-    return redirect(301, newDomain + route.path);
-  }
-
   const redirects = require('./301_sites/' + site_id + '.js')
 
   const matchedRedirect = redirects.default.find(([oldPath]) => oldPath === route.path);
+
+  const appendQueryString = (url, query) => {
+    const params = new URLSearchParams(query);
+    return params.toString() ? `${url}?${params}` : url;
+  }
+
+  if (route.path.startsWith('/reservation')) {
+    const newDomain = process.env.OLD_DOMAIN;
+    const newPath = newDomain + route.path;
+    return redirect(301, appendQueryString(newPath, route.query));
+  }
+
+  if (route.path.startsWith('/odeme-yap')) {
+    const newDomain = process.env.OLD_DOMAIN;
+    const newPath = newDomain + route.path;
+    return redirect(301, appendQueryString(newPath, route.query));
+  }
 
   if (matchedRedirect) {
     const [, newPath] = matchedRedirect;
