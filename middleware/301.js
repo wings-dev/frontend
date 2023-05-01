54e6f6c2e869902e3cbfc3e4654c07b6fa460385
max <ahmet.ugur@std.gau.edu.tr>
2023-05-01 10:55:38 +0300

    wilcard 301

--- middleware/301.js
+++ middleware/301.js
@@ -1,9 +1,15 @@
 export default function ({route, redirect}) {
   let site_id = process.env.SITE;
 
+  if (route.path.startsWith('/reservation')) {
+    const newDomain = process.env.OLD_DOMAIN;
+    return redirect(301, newDomain + route.path);
+  }
+
   const redirects = require('./301_sites/' + site_id + '.js')
 
   const matchedRedirect = redirects.default.find(([oldPath]) => oldPath === route.path);
+
   if (matchedRedirect) {
     const [, newPath] = matchedRedirect;
     return redirect(301, newPath);
